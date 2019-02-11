34d2ef90a22acc59888e418aa51fea1528130c17
Nguyen Van Phuc <phucnv9985@yahoo.com>
2019-02-11 22:06:12 +0700

    spring jdbc findAll

--- src/main/java/com/in28minutes/database/databasedemo/DatabaseDemoApplication.java
+++ src/main/java/com/in28minutes/database/databasedemo/DatabaseDemoApplication.java
@@ -1,13 +1,27 @@
 package com.in28minutes.database.databasedemo;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
-public class DatabaseDemoApplication {
+import com.in28minutes.database.databasedemo.jdbc.PersonJdbcDao;
 
+@SpringBootApplication
+public class DatabaseDemoApplication implements CommandLineRunner {
+	private Logger logger = LoggerFactory.getLogger(this.getClass());
+	
+	@Autowired
+	private PersonJdbcDao personJdbcDao;
 	public static void main(String[] args) {
 		SpringApplication.run(DatabaseDemoApplication.class, args);
+	}
+
+	@Override
+	public void run(String... args) throws Exception {
+		logger.info("All users: {}", personJdbcDao.findAll());
 	}
 
 }
