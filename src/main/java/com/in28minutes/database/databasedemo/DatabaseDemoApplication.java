498faca2f54e68a5b0fff48fa1b596c9e3d1832a
Nguyen Van Phuc <phucnv9985@yahoo.com>
2019-02-11 22:43:06 +0700

    findById

--- src/main/java/com/in28minutes/database/databasedemo/DatabaseDemoApplication.java
+++ src/main/java/com/in28minutes/database/databasedemo/DatabaseDemoApplication.java
@@ -22,6 +22,7 @@ public class DatabaseDemoApplication implements CommandLineRunner {
 	@Override
 	public void run(String... args) throws Exception {
 		logger.info("All users: {}", personJdbcDao.findAll());
+		logger.info("User id 10001: {}", personJdbcDao.findById(10001));
 	}
 
 }
